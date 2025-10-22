92c65b93e64377cb5135c6df6ae092ec316cd1dc
Benjaminske <siffertbenjamin@gmail.com>
2025-10-22 08:10:50 +0200

    Revert "Delete Programming directory"
    This reverts commit 0b8e625059cd7cbb450c1e7e832467061c89a39b.

---
+++ Programming/programmakes/les1.py
@@ -0,0 +1,7 @@
+import random
+
+def randomizer(x):
+     return random.uniform(0, x)
+    
+print(randomizer(50))
+
---
+++ Programming/programmakes/stringtest.py
@@ -0,0 +1,8 @@
+name1 = "Jef"
+name2 = "jos"
+name3 = "jan"
+nr1 = 1.45678978767876789
+nr2 = 2 == 2
+nr3 = 3
+
+print("abcdefg"[1:4])
