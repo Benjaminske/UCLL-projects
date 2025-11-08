38933246bfac80cb9a63f2009abc5c495625c76b
Benjaminske <siffertbenjamin@gmail.com>
2025-11-09 00:19:18 +0100

    tuple oef

--- Programming/cha/coursematerial_2526/07-tuples/02-assignment-split-name/student.py
+++ Programming/cha/coursematerial_2526/07-tuples/02-assignment-split-name/student.py
@@ -1,1 +1,6 @@
-# write your code here
+def split_name(name):
+    new_tuple = tuple(name.split())
+    return new_tuple
+
+print(split_name("Walter White"))
+    
--- Programming/cha/coursematerial_2526/07-tuples/04-assignment-all-equal/student.py
+++ Programming/cha/coursematerial_2526/07-tuples/04-assignment-all-equal/student.py
@@ -1,1 +1,10 @@
-# write your code here
+
+def all_equal(xs):
+    for i in range(len(xs) -1): 
+        if xs[i] != xs[i+1]:
+            return False
+    else :
+        return True
+
+        
+print(all_equal([1, 1, 1, 1, 2, 1, 1]))
