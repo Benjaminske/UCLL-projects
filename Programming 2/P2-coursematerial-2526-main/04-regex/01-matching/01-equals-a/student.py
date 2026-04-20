1ac1e5055111ef2b136b277eb559ee5b0cb446fe
Benjamin <siffertbenjamin@gmail.com>
2026-04-20 09:57:07 +0200

    regex oef
    regex oefening 12 af

--- Programming 2/P2-coursematerial-2526-main/04-regex/01-matching/01-equals-a/student.py
+++ Programming 2/P2-coursematerial-2526-main/04-regex/01-matching/01-equals-a/student.py
@@ -1,1 +1,6 @@
-# Write your code here
+import re
+def equals_a(string):
+    if re.fullmatch('a', string):
+        return True
+    
+print(equals_a("A"))
--- Programming 2/P2-coursematerial-2526-main/04-regex/01-matching/08-ten-times-abc/student.py
+++ Programming 2/P2-coursematerial-2526-main/04-regex/01-matching/08-ten-times-abc/student.py
@@ -1,1 +1,5 @@
-# Write your code here
+import re
+def ten_times_abc(string):
+    return re.fullmatch(r'(abc){10}', string)
+
+print(ten_times_abc("abcabcabcabcabcabcabcabcabcabc"))
